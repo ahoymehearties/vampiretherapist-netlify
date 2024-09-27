7b2456d99053a3a430470646f49c331b3c5a1970
Cyrus Nemati <cyrus.nemati@gmail.com>
2024-09-28 00:50:11 +0200

    bad discord link

--- components/Navbar.js
+++ components/Navbar.js
@@ -36,7 +36,7 @@ export default function Navbar() {
                     </a>
                 </li>
                 <li style={{ margin: "0 1rem" }}>
-                    <a href="https://discord.gg/yTfcZgXC" target="_blank" rel="noopener noreferrer">
+                    <a href="https://discord.gg/JM7Xr3zp7V" target="_blank" rel="noopener noreferrer">
                         <i className="fab fa-discord social-icon"></i>
                     </a>
                 </li>
